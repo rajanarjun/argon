ac40ec3a225d9395cb5ce9c1523a0087ca4cee4a
Arjun Rajan <arjunrajan991@gmail.com>
2025-09-18 00:53:07 +0530

    feat: basic file ingestion with .ar extension

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <filesystem>
 #include "lexer.h"
 
 
 int main(int argc, char** argv)
 {
+    if (argc < 2)
+    {
+        std::cerr << "   argon: \033[1;31m (fatal error)\033[0m no input files" << std::endl;
+        std::cout << "Compilation Terminated" << std::endl;
+        return 1;
+    }
+
+    std::filesystem::path _file;
+    int file_found = 0;
+    for (int i = 0; i < argc; i++)
+    {
+        _file = argv[i];
+        if (_file.extension() == ".ar")
+        {
+            file_found = 1;
+            break;
+        }
+    }
+
+    if (file_found == 0)
+    {
+        std::cerr << "   argon: file format not recognized" << std::endl;
+        return 1;
+    }
+    else
+    {
+        std::cout << "   argon: compiling file: " << _file << std::endl;
+    }
+
     return 0;
 }
