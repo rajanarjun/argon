f84b972d3bb8c2b45d67ee774937ffdcb6bfec8a
Arjun Rajan <arjunrajan991@gmail.com>
2025-09-18 01:17:42 +0530

    comments and some formatting

--- compiler/src/lexer.cpp
+++ compiler/src/lexer.cpp
@@ -1,7 +1,16 @@
+/*
+ * Lexical Analyzer
+ *
+ * First step of the compiler:
+ * This file will take in the source code as a
+ * series of characters and group it into tokens.
+ * 
+ */
+
 #include "lexer.h"
 
 
-int lexer()
-{
+int lexer() {
+
     return 0;
 }
--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -3,35 +3,30 @@
 #include "lexer.h"
 
 
-int main(int argc, char** argv)
-{
-    if (argc < 2)
-    {
+int main(int argc, char** argv) {
+
+    if (argc < 2) {
         std::cerr << "   argon: \033[1;31m (fatal error)\033[0m no input files" << std::endl;
         std::cout << "Compilation Terminated" << std::endl;
         return 1;
     }
 
-    std::filesystem::path _file;
+    std::filesystem::path _FILE;
     int file_found = 0;
-    for (int i = 0; i < argc; i++)
-    {
-        _file = argv[i];
-        if (_file.extension() == ".ar")
-        {
+
+    for (int i = 0; i < argc; i++) {
+        _FILE = argv[i];
+        if (_FILE.extension() == ".ar") {
             file_found = 1;
             break;
         }
     }
 
-    if (file_found == 0)
-    {
+    if (file_found == 0) {
         std::cerr << "   argon: file format not recognized" << std::endl;
         return 1;
-    }
-    else
-    {
-        std::cout << "   argon: compiling file: " << _file << std::endl;
+    } else {
+        std::cout << "   argon: compiling file: " << _FILE << std::endl;
     }
 
     return 0;
