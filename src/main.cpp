1c4329d90a7d649b04b64d72effa73b5c614dc29
Arjun Rajan <arjunrajan991@gmail.com>
2025-10-08 00:01:10 +0530

    using std namespace manually, lexer object initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include "lexer.hpp"
 
 
-using namespace std;
+std::string get_file_contents(const std::string& filename) {
 
-
-string get_file_contents(const string& filename) {
-
-    ifstream input_file(filename, ios::binary);
+    std::ifstream input_file(filename, std::ios::binary);
 
     if (!input_file) {
-        cerr << "   argon: Failed to open file: " << filename << endl;
+        std::cerr << "   argon: Failed to open file: " << filename << std::endl;
         return NULL;
     }
 
-    input_file.seekg(0, ios::end);
+    input_file.seekg(0, std::ios::end);
     streamsize fsize = input_file.tellg();
-    input_file.seekg(0, ios::beg);
+    input_file.seekg(0, std::ios::beg);
 
-    string contents(fsize, '\0');
+    std::string contents(fsize, '\0');
     if (!input_file.read(&contents[0], fsize)) {
-        cerr << "   argon: Failed to read file: " << filename << endl;
+        std::cerr << "   argon: Failed to read file: " << filename << std::endl;
         return NULL;
     }
 
@@ -32,18 +30,18 @@ string get_file_contents(const string& filename) {
 int main(int argc, char** argv) {
 
     if (argc < 2) {
-        cerr << "   argon: \033[1;31m (fatal error)\033[0m no input files" << endl;
-        cout << "Compilation Terminated" << endl;
+        std::cerr << "   argon: \033[1;31m (fatal error)\033[0m no input files" << std::endl;
+        std::cout << "Compilation Terminated" << std::endl;
         return 1;
     }
 
-    string source_file;
+    std::string source_file;
     int file_found = 0;
 
     for (int i = 0; i < argc; i++) {
         source_file = argv[i];
-        size_t pos = source_file.find_last_of('.');
-        if (pos != string::npos) {
+        std::size_t pos = source_file.find_last_of('.');
+        if (pos != std::string::npos) {
             if (source_file.substr(pos + 1)== "ar") {
                 file_found = 1;
                 break;
@@ -52,14 +50,13 @@ int main(int argc, char** argv) {
     }
 
     if (file_found == 0) {
-        cerr << "   argon: file format not recognized" << endl;
+        std::cerr << "   argon: file format not recognized" << std::endl;
         return 1;
     }
 
-    string source_file_contents = get_file_contents(source_file);
+    std::string source_file_contents = get_file_contents(source_file);
 
-    cout << source_file_contents << endl;
-   
+    Lexer lexer(source_file_contents);
 
     return 0;
 }
