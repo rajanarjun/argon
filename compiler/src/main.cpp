2791849c7819614da1969726b0b4ea0553281ff9
Arjun Rajan <arjunrajan991@gmail.com>
2025-10-07 19:19:25 +0530

    read file contents

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
 #include "lexer.hpp"
 
+
 using namespace std;
+
+
+string get_file_contents(const string& filename) {
+
+    ifstream input_file(filename, ios::binary);
+
+    if (!input_file) {
+        cerr << "   argon: Failed to open file: " << filename << endl;
+        return NULL;
+    }
+
+    input_file.seekg(0, ios::end);
+    streamsize fsize = input_file.tellg();
+    input_file.seekg(0, ios::beg);
+
+    string contents(fsize, '\0');
+    if (!input_file.read(&contents[0], fsize)) {
+        cerr << "   argon: Failed to read file: " << filename << endl;
+        return NULL;
+    }
+
+    return contents;
+}
+
 
 int main(int argc, char** argv) {
 
@@ -11,14 +37,14 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    string sourceFile;
+    string source_file;
     int file_found = 0;
 
     for (int i = 0; i < argc; i++) {
-        sourceFile = argv[i];
-        size_t pos = sourceFile.find_last_of('.');
+        source_file = argv[i];
+        size_t pos = source_file.find_last_of('.');
         if (pos != string::npos) {
-            if (sourceFile.substr(pos + 1)== "ar") {
+            if (source_file.substr(pos + 1)== "ar") {
                 file_found = 1;
                 break;
             }
@@ -30,8 +56,9 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    string source_file_contents = get_file_contents(source_file);
 
-    
+    cout << source_file_contents << endl;
    
 
     return 0;
