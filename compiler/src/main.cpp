0fc922143ab37ac78ee45e96557a42f36b7bfd55
Arjun Rajan <arjunrajan991@gmail.com>
2025-10-07 18:32:24 +0530

    checking file extension manually

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
-#include <filesystem>
-#include "lexer.h"
+#include "lexer.hpp"
 
+using namespace std;
 
 int main(int argc, char** argv) {
 
     if (argc < 2) {
-        std::cerr << "   argon: \033[1;31m (fatal error)\033[0m no input files" << std::endl;
-        std::cout << "Compilation Terminated" << std::endl;
+        cerr << "   argon: \033[1;31m (fatal error)\033[0m no input files" << endl;
+        cout << "Compilation Terminated" << endl;
         return 1;
     }
 
-    std::filesystem::path _FILE;
+    string sourceFile;
     int file_found = 0;
 
     for (int i = 0; i < argc; i++) {
-        _FILE = argv[i];
-        if (_FILE.extension() == ".ar") {
-            file_found = 1;
-            break;
+        sourceFile = argv[i];
+        size_t pos = sourceFile.find_last_of('.');
+        if (pos != string::npos) {
+            if (sourceFile.substr(pos + 1)== "ar") {
+                file_found = 1;
+                break;
+            }
         }
     }
 
     if (file_found == 0) {
-        std::cerr << "   argon: file format not recognized" << std::endl;
+        cerr << "   argon: file format not recognized" << endl;
         return 1;
-    } else {
-        std::cout << "   argon: compiling file: " << _FILE << std::endl;
     }
+
+
+    
+   
 
     return 0;
 }
