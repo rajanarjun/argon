f60eedb8c862c33e414846c30ed89240c2e7de69
Arjun Rajan <arjunrajan991@gmail.com>
2025-10-07 19:46:24 +0530

    directory reshuffle

---
+++ build/CMakeFiles/argon.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/argon.dir/src/lexer.cpp.o"
+  "CMakeFiles/argon.dir/src/lexer.cpp.o.d"
+  "CMakeFiles/argon.dir/src/main.cpp.o"
+  "CMakeFiles/argon.dir/src/main.cpp.o.d"
+  "argon"
+  "argon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/argon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/argon.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for argon.
+# This may be replaced when dependencies are built.
