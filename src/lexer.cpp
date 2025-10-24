6317f54b35079916cb1287d9dadd2d4ea30a9e40
Arjun Rajan <arjunrajan991@gmail.com>
2025-10-25 02:23:06 +0530

    formatter settings

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -10,8 +10,11 @@
 #include "lexer.hpp"
 #include <iostream>
 
-Lexer::Lexer(const std::string &source_contents)
-    : input_text(source_contents), cpos(0), cline(1), ccol(1) {
-
-  std::cout << source_contents << std::endl;
+Lexer::Lexer(const std::string &source_contents) 
+    : input_text(source_contents), 
+      cpos(0), 
+      cline(1), 
+      ccol(1)
+{
+    std::cout << source_contents << std::endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,57 +5,57 @@
 
 std::string get_file_contents(const std::string &filename) {
 
-  std::ifstream input_file(filename, std::ios::binary);
+    std::ifstream input_file(filename, std::ios::binary);
 
-  if (!input_file) {
-    std::cerr << "   argon: Failed to open file: " << filename << std::endl;
-    return "";
-  }
+    if (!input_file) {
+        std::cerr << "   argon: Failed to open file: " << filename << std::endl;
+        return "";
+    }
 
-  input_file.seekg(0, std::ios::end);
-  std::streamsize fsize = input_file.tellg();
-  input_file.seekg(0, std::ios::beg);
+    input_file.seekg(0, std::ios::end);
+    std::streamsize fsize = input_file.tellg();
+    input_file.seekg(0, std::ios::beg);
 
-  std::string contents(fsize, '\0');
-  if (!input_file.read(&contents[0], fsize)) {
-    std::cerr << "   argon: Failed to read file: " << filename << std::endl;
-    return "";
-  }
+    std::string contents(fsize, '\0');
+    if (!input_file.read(&contents[0], fsize)) {
+        std::cerr << "   argon: Failed to read file: " << filename << std::endl;
+        return "";
+    }
 
-  return contents;
+    return contents;
 }
 
 int main(int argc, char **argv) {
 
-  if (argc < 2) {
-    std::cerr << "   argon: \033[1;31m (fatal error)\033[0m no input files"
-              << std::endl;
-    std::cout << "Compilation Terminated" << std::endl;
-    return 1;
-  }
-
-  std::string source_file;
-  int file_found = 0;
-
-  for (int i = 0; i < argc; i++) {
-    source_file = argv[i];
-    std::size_t pos = source_file.find_last_of('.');
-    if (pos != std::string::npos) {
-      if (source_file.substr(pos + 1) == "ar") {
-        file_found = 1;
-        break;
-      }
+    if (argc < 2) {
+        std::cerr << "   argon: \033[1;31m (fatal error)\033[0m no input files"
+            << std::endl;
+        std::cout << "Compilation Terminated" << std::endl;
+        return 1;
     }
-  }
 
-  if (file_found == 0) {
-    std::cerr << "   argon: file format not recognized" << std::endl;
-    return 1;
-  }
+    std::string source_file;
+    int file_found = 0;
 
-  std::string source_file_contents = get_file_contents(source_file);
+    for (int i = 0; i < argc; i++) {
+        source_file = argv[i];
+        std::size_t pos = source_file.find_last_of('.');
+        if (pos != std::string::npos) {
+            if (source_file.substr(pos + 1) == "ar") {
+                file_found = 1;
+                break;
+            }
+        }
+    }
 
-  Lexer lexer(source_file_contents);
+    if (file_found == 0) {
+        std::cerr << "   argon: file format not recognized" << std::endl;
+        return 1;
+    }
 
-  return 0;
+    std::string source_file_contents = get_file_contents(source_file);
+
+    Lexer lexer(source_file_contents);
+
+    return 0;
 }
