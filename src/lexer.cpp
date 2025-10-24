46eda0089140671b618acae5476f326ce01fce6a
Arjun Rajan <arjunrajan991@gmail.com>
2025-10-25 00:17:18 +0530

    llvm format convention

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -4,16 +4,14 @@
  * First step of the compiler:
  * This file will take in the source code as a
  * series of characters and group it into tokens.
- * 
+ *
  */
 
-#include <iostream>
 #include "lexer.hpp"
+#include <iostream>
 
-
-Lexer::Lexer(const std::string& source_contents)
+Lexer::Lexer(const std::string &source_contents)
     : input_text(source_contents), cpos(0), cline(1), ccol(1) {
 
-        std::cout << source_contents << std::endl;
+  std::cout << source_contents << std::endl;
 }
-
--- src/lexer.hpp
+++ src/lexer.hpp
@@ -3,40 +3,39 @@
 #include <string>
 
 enum class TokenType {
-    IDENTIFIER,  // x, y ,c, s, add
-    CHAR,        // 'a'
-    STRING,      // "hello world"
-    NUMBER,      // 12, 99.9
-    LET,         // let
-    EQUAL,       // =
-    END,         // end
-    IF,          // if
-    ELSE,        // else
-    QMARK,       // ?
-    PRINT,       // print
-    LESS,        // <
-    GREATER,     // >
-    COLON,       // :
-    PLUS,        // +
-    MINUS,       // -
-    STAR,        // *
-    SLASH,       // /
-    HASH,        // #
-    NUM_TYPE,    // num
-    DEC_TYPE,    // dec
-    ALP_TYPE,    // alp
-    TEXT_TYPE,   // text
-    END_OF_FILE  // END OF FILE
+  IDENTIFIER, // x, y ,c, s, add
+  CHAR,       // 'a'
+  STRING,     // "hello world"
+  NUMBER,     // 12, 99.9
+  LET,        // let
+  EQUAL,      // =
+  END,        // end
+  IF,         // if
+  ELSE,       // else
+  QMARK,      // ?
+  PRINT,      // print
+  LESS,       // <
+  GREATER,    // >
+  COLON,      // :
+  PLUS,       // +
+  MINUS,      // -
+  STAR,       // *
+  SLASH,      // /
+  HASH,       // #
+  NUM_TYPE,   // num
+  DEC_TYPE,   // dec
+  ALP_TYPE,   // alp
+  TEXT_TYPE,  // text
+  END_OF_FILE // END OF FILE
 };
 
-
 class Lexer {
-    private:
-        std::string input_text;
-        int cpos;
-        int cline;
-        int ccol;
-    public:
-        Lexer(const std::string& source_contents);
+private:
+  std::string input_text;
+  int cpos;
+  int cline;
+  int ccol;
 
+public:
+  Lexer(const std::string &source_contents);
 };
