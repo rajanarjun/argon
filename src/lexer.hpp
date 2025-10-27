4b967c8d562886316f6618604ccffee56a2779e4
Arjun Rajan <arjunrajan991@gmail.com>
2025-10-28 00:39:14 +0530

    lexer constructor intialization in header, public before private, struct defaults and ordering, TODOs

--- src/lexer.hpp
+++ src/lexer.hpp
@@ -1,3 +1,8 @@
+/* TODO:
+   Need to add more token types as according to lexer.cpp implemetation
+   */
+
+
 #pragma once
 
 #include <string>
@@ -29,20 +34,28 @@ enum class TokenType {
     PUNCTUATION_OPENPAREN,
     PUNCTUATION_CLOSEPAREN,
     PUNCTUATION_COLON,
-    PUNCTUATION_HASH,
+    PUNCTUATION_COMMENT,
     END_OF_FILE
 };
 
 
 struct Token {
     TokenType type;
-    std::string value;
-    int line;
-    int column;
+    int line = 0;
+    int column = 0;
+    std::string value = "";
 };
 
 
 class Lexer {
+public:
+    Lexer(const std::string &source_contents) : input_text(source_contents), 
+                                                current_position(0), 
+                                                current_line(1), 
+                                                current_column(1) {}
+
+    void generate_tokens();
+
 private:
     std::string input_text;
     TokenType current_token;
@@ -50,9 +63,4 @@ private:
     int current_line;
     int current_column;
     vector<Token> token_stream;
-
-public:
-    Lexer(const std::string &source_contents);
-    
-    void tokenize();
 };
